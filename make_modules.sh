a66014fa4b44d591090c8c99ccc5474b9b7290e9
chuM <chuM@163.com>
2014-08-13 13:01:56 +0800

    modify make_modulese.sh make_uimage.sh

--- make_modules.sh
+++ make_modules.sh
@@ -2,4 +2,4 @@
 
 cp .config config_now
 make modules
-make modules_install INSTALL_MOD_PATH=/home/barnard/work/rootfs
+make modules_install INSTALL_MOD_PATH=/home/barnard/work/board_9G45/rootfs
--- make_uimage.sh
+++ make_uimage.sh
@@ -3,3 +3,11 @@
 cp .config config_now
 make uImage
 cp arch/arm/boot/uImage /opt/tftpboot/uImage
+
+
+echo "cp arch/arm/boot/uImage /mnt/hgfs/windown-linux/board_9g25/gateway_images"
+cp arch/arm/boot/uImage /mnt/hgfs/windown-linux/board_9g25/gateway_images
+
+
+
+
